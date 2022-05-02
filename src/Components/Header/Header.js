300aecc42d752f043420fce32aa80eb80252b0ed
JEBA <fawjiajeba@gmail.com>
2022-05-02 13:10:04 +0600

    commit message

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css'
+const header = () => {
+      return (
+            <nav className='header'>
+                  <h1>Choose your course</h1>
+            </nav>
+      );
+};
+
+export default header;
